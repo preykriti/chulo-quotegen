74461ebaa8ab0226cae7703286875a8b33d535d9
preykriti <prakriteeshrestha2@gmail.com>
2025-05-10 17:01:15 +0545

    font size changing function added

--- script.js
+++ script.js
@@ -1,104 +1,4 @@
-const quotes = [
-  {
-    id: 1,
-    quote:
-      "The only limit to our realization of tomorrow is our doubts of today.",
-    author: "Franklin D. Roosevelt",
-    category: "motivational",
-  },
-  {
-    id: 2,
-    quote:
-      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-    author: "Winston Churchill",
-    category: "motivational",
-  },
-  {
-    id: 3,
-    quote: "Don’t watch the clock; do what it does. Keep going.",
-    author: "Sam Levenson",
-    category: "motivational",
-  },
-  {
-    id: 4,
-    quote: "Everything you’ve ever wanted is on the other side of fear.",
-    author: "George Addair",
-    category: "motivational",
-  },
-  {
-    id: 5,
-    quote: "Start where you are. Use what you have. Do what you can.",
-    author: "Arthur Ashe",
-    category: "motivational",
-  },
-
-  {
-    id: 6,
-    quote: "Love is composed of a single soul inhabiting two bodies.",
-    author: "Aristotle",
-    category: "love",
-  },
-  {
-    id: 7,
-    quote: "To love and be loved is to feel the sun from both sides.",
-    author: "David Viscott",
-    category: "love",
-  },
-  {
-    id: 8,
-    quote:
-      "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-    author: "Dr. Seuss",
-    category: "love",
-  },
-  {
-    id: 9,
-    quote:
-      "Love recognizes no barriers. It jumps hurdles, leaps fences, penetrates walls to arrive at its destination full of hope.",
-    author: "Maya Angelou",
-    category: "love",
-  },
-  {
-    id: 10,
-    quote:
-      "Being deeply loved by someone gives you strength, while loving someone deeply gives you courage.",
-    author: "Lao Tzu",
-    category: "love",
-  },
-
-  {
-    id: 11,
-    quote: "People who love to eat are always the best people.",
-    author: "Julia Child",
-    category: "food",
-  },
-  {
-    id: 12,
-    quote:
-      "One cannot think well, love well, sleep well, if one has not dined well.",
-    author: "Virginia Woolf",
-    category: "food",
-  },
-  {
-    id: 13,
-    quote: "Food is symbolic of love when words are inadequate.",
-    author: "Alan D. Wolfelt",
-    category: "food",
-  },
-  {
-    id: 14,
-    quote: "Life is uncertain. Eat dessert first.",
-    author: "Ernestine Ulmer",
-    category: "food",
-  },
-  {
-    id: 15,
-    quote: "First we eat, then we do everything else.",
-    author: "M.F.K. Fisher",
-    category: "food",
-  },
-];
-
+import {quotes} from "./data.js"
 
 const quoteText = document.getElementById("quote-text");
 const quoteAuthor = document.getElementById("quote-author");
@@ -106,9 +6,31 @@ const previousBtn = document.getElementById("previous-btn");
 const randomBtn = document.getElementById("random-btn");
 const nextBtn = document.getElementById("next-btn");
 const category = document.getElementById("category");
+const increaseFont = document.getElementById("increase-font");
+const decreaseFont = document.getElementById("decrease-font");
+const displayFont = document.getElementById("font-size-display");
 
 let i = 0;
 let chosenQuotes = [];
+let chosenSize = 16;
+
+increaseFont.addEventListener("click", ()=>{
+  if(chosenSize <52){
+    chosenSize = chosenSize + 2;
+    quoteText.style.fontSize = `${chosenSize}px`;
+    quoteAuthor.style.fontSize = `${chosenSize-5}px`;
+    displayFont.textContent = `${chosenSize}px`;
+  }
+});
+
+decreaseFont.addEventListener("click", ()=>{
+  if (chosenSize > 16) {
+    chosenSize = chosenSize - 2;
+    quoteText.style.fontSize = `${chosenSize}px`;
+    quoteAuthor.style.fontSize = `${chosenSize - 5}px`;
+    displayFont.textContent = `${chosenSize}px`;
+  }
+})
 
 function updateQuotes(){
     const chosenCategory = category.value;
